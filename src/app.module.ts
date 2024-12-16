6d0af7b3f44ff403e455d77d1cd62910222c0307
Anathapindika Muliawan <anathapindika.kian@gmail.com>
2024-12-16 17:20:03 +0700

    Add HTTP exception filter to handle errors globally and update app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,21 @@ import {
 } from "@nestjs/common";
 import { LoggerMiddleware } from "./shared/middleware/logger.middleware";
 import { DatabaseModule } from "./database/database.module";
-import appConfig from "./modules";
-import { APP_INTERCEPTOR } from "@nestjs/core";
+import appModule from "./modules";
+import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
 import { ResponseInterceptor } from "./shared/interceptors/response.interceptor";
+import { HttpExceptionFilter } from "./shared/filters/http-exception.filter";
 
 @Module({
-  imports: [DatabaseModule, ...appConfig.modules],
+  imports: [DatabaseModule, ...appModule.modules],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
     },
   ],
 })
