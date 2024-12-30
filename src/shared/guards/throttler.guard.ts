418dcbd34f38de63502bcd77ed1c279fc43a93ac
Anathapindika Muliawan <anathapindika.kian@gmail.com>
2024-12-30 15:29:51 +0700

    Remove console log from ThrottlerRateLimitGuard to clean up request tracking

--- src/shared/guards/throttler.guard.ts
+++ src/shared/guards/throttler.guard.ts
@@ -1,10 +1,9 @@
-import { Injectable, ExecutionContext } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { ThrottlerGuard } from "@nestjs/throttler";
 
 @Injectable()
 export class ThrottlerRateLimitGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): Promise<string> {
-    console.log(req.user);
     return super.getTracker(req);
   }
 }
