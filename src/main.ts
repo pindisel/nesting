63842261b3154b323d28c91f1364270f04161daf
Anathapindika Muliawan <72156714+pindisel@users.noreply.github.com>
2024-12-31 10:30:32 +0700

    Add Swagger integration and API documentation to user and auth DTOs

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { EnvConfig } from "./config/env.config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 const PORT = new EnvConfig().serverPort;
+const VERSION = process.env.npm_package_version;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
   app.enableCors();
 
-  Logger.log(`Server running on port: ${PORT}`, "Bootstrap");
-
+  // Enable validation pipe for all requests
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -22,6 +25,24 @@ async function bootstrap() {
     }),
   );
 
+  // Enable Swagger
+  const config = new DocumentBuilder()
+    .setTitle("NestJS Template API")
+    .setDescription("NestJS Template API description")
+    .setVersion(VERSION)
+    .addBearerAuth()
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, {
+      deepScanRoutes: true,
+    });
+  SwaggerModule.setup("api", app, documentFactory, {
+    customSiteTitle: "NestJS Template API",
+    jsonDocumentUrl: "/api-json",
+  });
+
+  // Start the server
+  Logger.log(`Server running on port: ${PORT}`, "Bootstrap");
   await app.listen(PORT);
 }
 bootstrap();
--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,18 +1,23 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateUserDto } from "./create-user.dto";
 import { IsEmail, IsString } from "class-validator";
+import { ApiPropertyOptional } from "@nestjs/swagger";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
+  @ApiPropertyOptional()
   name: string;
 
   @IsString()
   @IsEmail()
+  @ApiPropertyOptional()
   email: string;
 
   @IsString()
+  @ApiPropertyOptional()
   password: string;
 
   @IsString()
+  @ApiPropertyOptional()
   role: string;
 }
